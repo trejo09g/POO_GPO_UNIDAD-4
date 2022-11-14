20552d24ed9ae2f66cf4c069fb701740b217b256
invitado <invitado@LAB010208.itchii.lab>
2022-11-14 13:35:31 -0700

    Pelon

---
+++ src/Unidad_1/ArrayDeNombres.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Unidad_1;
+
+/**
+ *
+ * @author invitado
+ */
+public class ArrayDeNombres {
+    
+    public static void main(String[] args) {
+        /*String[]nombre = new String[5];
+        
+        nombre[0] = "Luis";
+        nombre[1] = "Maria";
+        nombre[2] = "Carlos";
+        nombre[3] = "Jose";
+        nombre[4] = "Ismael";
+        
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(nombre[i]);
+        }*/
+        String[]arg = new String[2];
+        System.out.println("Cantidad de parametros: " + arg.length);
+        System.out.println(arg[0]);
+        System.out.println(arg[1]);
+        }
+    }
+
---
+++ src/estructura/de/datos/EstructuraDeDatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package estructura.de.datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EstructuraDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
